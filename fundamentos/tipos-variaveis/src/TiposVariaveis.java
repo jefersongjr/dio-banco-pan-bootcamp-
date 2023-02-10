b210c3e7acc3664e05c4ac867732b66154febbd0
jefersongjr <jeferson_gjr@hotmail.com>
2023-02-10 10:27:57 -0300

    :sparkles: feat: estudo de possível erro +  constantes

--- fundamentos/tipos-variaveis/src/TiposVariaveis.java
+++ fundamentos/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double numero = 24440;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        numeroCurto = (short) numeroNormal; // o cast serve pra forçar um valor
+
+        final double PI = 3.14; 
+        System.out.println(numero);
+        System.out.println(PI);
     }
 }
