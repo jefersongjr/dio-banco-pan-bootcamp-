a986bf17f9559813890b708e050be2e8a19c64a9
jefersongjr <jeferson_gjr@hotmail.com>
2023-02-10 11:48:22 -0300

    :sparkles: feat:  estudando operadores em Java

---
+++ fundamentos/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+
+    public static void main(String[] args) throws Exception {
+        int numero = 5;
+        System.out.println( numero);
+
+        numero = - numero;
+        System.out.println(numero);
+
+        numero = numero * -1;
+        System.out.println(numero);
+
+        System.out.println(numero ++); // incrementa depois 
+
+        System.out.println(numero);
+        
+        System.out.println(++ numero); // incrementa antes
+    }
+}
