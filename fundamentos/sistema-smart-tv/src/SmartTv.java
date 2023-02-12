c3a98882c699676e86fe7efef10437c2f942f3f8
jefersongjr <jeferson_gjr@hotmail.com>
2023-02-11 22:14:39 -0300

    :sparkles: feat: criando método para aumentar e diminuir volume

--- fundamentos/sistema-smart-tv/src/SmartTv.java
+++ fundamentos/sistema-smart-tv/src/SmartTv.java
@@ -3,6 +3,16 @@ public class SmartTv {
     int canal = 1;
     int volume = 25;
 
+    public void aumentarVolume() {
+        volume++;
+        System.out.println("Aumentando o volume para " + volume);
+    }
+
+    public void diminuiVolume() {
+        volume--;
+        System.out.println("Diminuindo o volume para " + volume);
+    }
+
     public void ligar() {
         ligada = true;
     }
--- fundamentos/sistema-smart-tv/src/Usuario.java
+++ fundamentos/sistema-smart-tv/src/Usuario.java
@@ -3,8 +3,18 @@ public class Usuario {
        
         SmartTv smarttv = new SmartTv();
 
+        smarttv.diminuiVolume();
+        smarttv.diminuiVolume();
+        smarttv.diminuiVolume();
+        smarttv.aumentarVolume();
+
+
         System.out.println("Tv está ligada"  + smarttv.ligada);
         System.out.println("Canal atual"  + smarttv.canal);
         System.out.println("Volume atual"  + smarttv.volume);
+
+        smarttv.desligar();
+
+        System.out.println("Tv está ligada"  + smarttv.ligada);
     }
 }
