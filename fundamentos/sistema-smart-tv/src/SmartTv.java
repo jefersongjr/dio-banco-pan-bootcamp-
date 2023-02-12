4749c5393b9137d73c5db7a6b9804f53d537d5b0
jefersongjr <jeferson_gjr@hotmail.com>
2023-02-11 22:09:17 -0300

    :sparkles: feat: criando método ligar e desligar

--- fundamentos/sistema-smart-tv/src/SmartTv.java
+++ fundamentos/sistema-smart-tv/src/SmartTv.java
@@ -2,4 +2,12 @@ public class SmartTv {
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
 }
